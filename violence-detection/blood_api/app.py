b889511ed3b6156d0a84e4caa2c7dc1c03bdc871
Jahanzaib Piracha <zaibjahan.200@gmail.com>
2026-07-02 16:44:44 +0500

    hi

--- violence-detection/blood_api/app.py
+++ violence-detection/blood_api/app.py
@@ -84,4 +84,4 @@ async def test_form():
 if __name__ == "__main__":
     import uvicorn
     # reload=False is recommended for production
-    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=False)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)

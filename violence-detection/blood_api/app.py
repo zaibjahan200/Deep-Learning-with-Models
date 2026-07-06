6705033cb412337a881b9ff4ece30372cbb430c9
Jahanzaib Piracha <zaibjahan.200@gmail.com>
2026-07-06 15:15:15 +0500

    push

--- violence-detection/blood_api/app.py
+++ violence-detection/blood_api/app.py
@@ -15,7 +15,7 @@ import uvicorn
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 # ---- Load model ----
-MODEL_PATH = "phase2_checkpoint.keras"
+MODEL_PATH = "phase2_checkpoint_best.keras"
 if not os.path.exists(MODEL_PATH):
     raise FileNotFoundError(f"Model file not found at {MODEL_PATH}")
 model = keras.models.load_model(MODEL_PATH)
